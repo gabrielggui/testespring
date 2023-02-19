a1e9d891f6fa5b9bad47053e34cd2dfc0fc3303a
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2023-02-19 18:05:54 -0300

    .

--- src/main/java/com/gabriel/spring/controller/PessoaController.java
+++ src/main/java/com/gabriel/spring/controller/PessoaController.java
@@ -3,13 +3,22 @@ package com.gabriel.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.gabriel.spring.model.Pessoa;
 import com.gabriel.spring.repository.PessoaRepository;
+
+/**
+ * Cadastrar pessoa
+ * listar pessoas
+ * Editar pessoa
+ * Deletar pessoa
+ */
 
 @Controller
 public class PessoaController {
@@ -34,4 +43,24 @@ public class PessoaController {
         return new ModelAndView("cadastro/cadastropessoa")
                 .addObject("pessoas", pessoaIterable);
     }
+
+    @GetMapping("/editarpessoa/{idpessoa}")
+    public ModelAndView editar(@PathVariable("idpessoa") Long id) {
+        Pessoa pessoa = pessoaRepository.findById(id).get();
+        return new ModelAndView("cadastro/editarpessoa")
+                .addObject("pessoaobj",pessoa);
+    }
+
+    @GetMapping("/excluirpessoa/{idpessoa}")
+    public ModelAndView excluir(@PathVariable("idpessoa") Long id) {
+        pessoaRepository.deleteById(id);
+        return listaDePessoas();
+    }
+    
+    @PostMapping("/buscarpessoa")
+    public ModelAndView buscar(@RequestParam("nomepesquisa") String nome) {
+        return new ModelAndView("cadastro/cadastropessoa")
+                .addObject("pessoas",pessoaRepository.findByName(nome));
+    }
+
 }
--- src/main/java/com/gabriel/spring/repository/PessoaRepository.java
+++ src/main/java/com/gabriel/spring/repository/PessoaRepository.java
@@ -1,5 +1,6 @@
 package com.gabriel.spring.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +11,6 @@ import com.gabriel.spring.model.Pessoa;
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
     
+    @Query("select p from Pessoa p where p.nome like %?1%")
+    public Iterable<Pessoa> findByName(String nome);
 }
